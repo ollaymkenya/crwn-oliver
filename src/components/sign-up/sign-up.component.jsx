943754d10e784190ef65a06681a4afa9f9417a2e
ollaymkenya <muriithioliver11@outlook.com>
2020-12-30 15:16:20 +0300

    Implemented firebase utils added ability to add users into firebase database

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+// Components
+import FormInput from "../form-input/form-input.components";
+import CustomButton from "../custom-button/custom-button.components";
+
+import { auth, createUserProfileDocuemnt } from '../../firebase/firebase.utils';
+
+import './sign-up.style.scss';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email, password, confirmPassword } = this.state;
+        if (password !== confirmPassword) {
+            alert("passwords do not match");
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            await createUserProfileDocuemnt(user, { displayName });
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        const { displayName, email, password, confirmPassword } = this.state;
+        return (
+            <div className="sign-up">
+                <h2 className="title">I do not have an account</h2>
+                <span className="sign-up-form" onSubmit={this.handleSubmit}>Sign up with your email or password</span>
+                <form className="sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormInput type="text" name="displayName" value={displayName} onChange={this.handleChange} label="Display Name" required />
+                    <FormInput type="email" name="email" value={email} onChange={this.handleChange} label="Email" required />
+                    <FormInput type="password" name="password" value={password} onChange={this.handleChange} label="Password" required />
+                    <FormInput type="password" name="confirmPassword" value={confirmPassword} onChange={this.handleChange} label="Confirm Password" required />
+                    <CustomButton type="submit">SIGN UP</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     measurementId: "G-P1TWL9WGNX"
 };
 
+export const createUserProfileDocuemnt = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.meessage);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth()
